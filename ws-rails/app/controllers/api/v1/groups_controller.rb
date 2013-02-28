76600839627b491d79ba7d6d87386c6f415e9dfc
Beany <zou.mingxin@gmail.com>
2013-02-28 21:30:33 +0800

    Fixed the api for adding group.

--- ws-rails/app/controllers/api/v1/groups_controller.rb
+++ ws-rails/app/controllers/api/v1/groups_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::GroupsController < Api::V1::BaseController
   before_filter
+
   def active
     respond_with(Group.where('due_date >= ?', Time.now))
   end
 
   def create
     restaurant = Restaurant.find(params[:restaurant_id])
-    group = Group.new(:user => current_user,
-                     :restaurant => restaurant).save!
-    respond_with(Group.all, :status => 201)
+    group = Group.create! :user => current_user, :restaurant => restaurant
+    render :json => group, :status => :created
   end
 end
