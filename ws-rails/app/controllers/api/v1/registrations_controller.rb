d17923b8a75a39a637e46471bac39f6a19362534
Beany <zou.mingxin@gmail.com>
2013-02-28 20:29:37 +0800

    Changed from auth_token to token.

--- ws-rails/app/controllers/api/v1/registrations_controller.rb
+++ ws-rails/app/controllers/api/v1/registrations_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RegistrationsController < Api::V1::BaseController
   def create
     user = User.new(params[:user])
     if user.save
-      render :json => user.as_json(:auth_token => user.authentication_token, :email => user.email), :status => 201
+      render :json => user.as_json(:token => user.authentication_token, :email => user.email), :status => 201
       return
     else
       warden.custom_failure!
