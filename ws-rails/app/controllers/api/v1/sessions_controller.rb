8c2bc478f04f0f546687540e6c5990b0c6a0e94a
Beany <zou.mingxin@gmail.com>
2013-03-02 10:27:52 +0800

    Return the name of user after login.

--- ws-rails/app/controllers/api/v1/sessions_controller.rb
+++ ws-rails/app/controllers/api/v1/sessions_controller.rb
@@ -12,7 +12,7 @@ module Api
 
         if user.valid_password?(params[:password])
           sign_in("user", user)
-          render :json => {:success => true, :token => user.authentication_token, :email => user.email}
+          render :json => {:success => true, :token => user.authentication_token, :email => user.email, :name => user.name}
           return
         end
         invalid_login_attempt
