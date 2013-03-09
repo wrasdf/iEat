e4ac53ac369c4454996a68d450b2ef2e15b91c58
Beany <zou.mingxin@gmail.com>
2013-03-09 10:37:43 +0800

    Fixed the api for order creation.

--- ws-rails/app/controllers/api/v1/orders_controller.rb
+++ ws-rails/app/controllers/api/v1/orders_controller.rb
@@ -1,23 +1,17 @@
+require 'oj'
+
 class Api::V1::OrdersController < Api::V1::BaseController
   before_filter
 
   def create
     group = Group.find(params[:group_id])
-    order = Order.create(:user => current_user, :group => group)
+    @order = Order.create(:user => current_user, :group => group)
 
-
-    puts "asdbf"
-    params[:dishes].each do |x|
-      puts x
+    Oj.load(params[:dishes]).each do |dish|
+      OrderDish.create(:order => @order, :dish_id => dish['id'], :quantity => dish['quantity'].to_i)
     end
-    #OrderDish.create()
 
-    puts 'hereeeeeeeeeeeeeehereeeeeeeeeeeeeehereeeeeeeeeeeeeehereeeeeeeeeeeeeehereeeeeeeeeeeeeehereeeeeeeeeeeeee'
-    if order.valid?
-      #respond_with(order, :location => api_v1_group_order_path(group, order))
-    else
+    render :file => 'rabl/order'
 
-      respond_with(order)
-    end
   end
 end
